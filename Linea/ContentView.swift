a554198b69f75ca6c7be7758d9b9f0c04b904ee6
fedorokonma <flapershf018@gmail.com>
2026-08-28 14:19:39 +0500

    Initial Commit

---
+++ Linea/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Linea
+//
+//  Created by Fedor Sherstnev on 28.08.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Linea/LineaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineaApp.swift
+//  Linea
+//
+//  Created by Fedor Sherstnev on 28.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct LineaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
